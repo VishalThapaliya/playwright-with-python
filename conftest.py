0c1686944c7fcca292dc2513cca34e67ace999e0
VishalThapaliya <vishal.thapaliya@gmail.com>
2026-09-15 22:19:55 +0200

    feat: enhance browser fixture to support headed and slow motion options

--- conftest.py
+++ conftest.py
@@ -2,9 +2,12 @@ import pytest
 from playwright.sync_api import sync_playwright
 
 @pytest.fixture(scope="session")
-def browser():
+def browser(request):
+    headed = request.config.getoption("--headed")
+    slow_mo = request.config.getoption("--slowmo")
+
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=not headed, slow_mo=slow_mo)
         yield browser
         browser.close()
 

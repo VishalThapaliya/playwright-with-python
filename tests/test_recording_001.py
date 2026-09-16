d70e62dd6689cfd2938aca989c6edca55ba17837
VishalThapaliya <vishal.thapaliya@gmail.com>
2026-09-16 10:07:23 +0200

    feat: add Playwright test recording script for automated browser interactions

---
+++ tests/test_recording_001.py
@@ -0,0 +1,39 @@
+# Generate Playwright code with recording
+# execute 'playwright codegen <URL location>' command on terminal
+# It will open a browser and will record all the steps we perform and generate code as per our steps performed
+
+# Example: Go to "saucedemo.com", login, add items to cart, remove item from cart, checkout
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://www.saucedemo.com/")
+    page.locator("[data-test=\"username\"]").click()
+    page.locator("[data-test=\"username\"]").fill("standard_user")
+    page.locator("[data-test=\"username\"]").press("Tab")
+    page.locator("[data-test=\"password\"]").fill("secret_sauce")
+    page.locator("[data-test=\"login-button\"]").click()
+    page.locator("[data-test=\"add-to-cart-sauce-labs-backpack\"]").click()
+    page.locator("[data-test=\"add-to-cart-sauce-labs-bike-light\"]").click()
+    page.locator("[data-test=\"shopping-cart-link\"]").click()
+    page.locator("[data-test=\"remove-sauce-labs-bike-light\"]").click()
+    page.locator("[data-test=\"checkout\"]").click()
+    page.locator("[data-test=\"firstName\"]").click()
+    page.locator("[data-test=\"firstName\"]").fill("Bishal")
+    page.locator("[data-test=\"firstName\"]").press("Tab")
+    page.locator("[data-test=\"lastName\"]").fill("Thapaliya")
+    page.locator("[data-test=\"lastName\"]").press("Tab")
+    page.locator("[data-test=\"postalCode\"]").fill("38000")
+    page.locator("[data-test=\"continue\"]").click()
+    page.locator("[data-test=\"finish\"]").click()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
